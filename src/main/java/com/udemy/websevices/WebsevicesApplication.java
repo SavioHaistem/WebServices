2ff965d28a3987cbd2ac9f81d04749ff0940a3b0
SavioHaistem <savio.dantas.5858@gmail.com>
2024-06-25 11:08:31 -0300

    feat: spring initializer commit

---
+++ src/main/java/com/udemy/websevices/WebsevicesApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.websevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsevicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/websevices/WebsevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.websevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
