38ec7fac4f7e39da4a6495d0b2468453544969c0
SavioHaistem <savio.dantas.5858@gmail.com>
2024-06-26 15:51:26 -0300

    refactor: setting dependencies

--- src/main/java/com/udemy/websevices/WebsevicesApplication.java
+++ src/main/java/com/udemy/websevices/WebsevicesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebsevicesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebsevicesApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/udemy/websevices/controllers/Main.java
@@ -0,0 +1,13 @@
+package com.udemy.websevices.controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Main {
+    @GetMapping("/main")
+    public ResponseEntity<?> getMain() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
