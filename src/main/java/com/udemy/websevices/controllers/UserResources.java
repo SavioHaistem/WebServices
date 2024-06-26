4bfa115b98f6313325ef7f0bc42976dc43193d88
SavioHaistem <savio.dantas.5858@gmail.com>
2024-06-26 16:25:50 -0300

    feat: user controller

---
+++ src/main/java/com/udemy/websevices/controllers/UserResources.java
@@ -0,0 +1,17 @@
+package com.udemy.websevices.controllers;
+
+import com.udemy.websevices.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+    @GetMapping
+    public ResponseEntity<User> getUser() {
+        User user = new User(1,"Leo","0800","entre.vaicachorra","minhasenha.fuleira");
+        return ResponseEntity.ok().body(user);
+    }
+}
